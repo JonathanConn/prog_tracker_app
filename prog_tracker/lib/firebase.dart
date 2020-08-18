fbb9aac0046e6d8c0fb06bbb6163a149d14ec6d3
Jonathan <jonathanconn29@gmail.com>
2020-08-18 13:44:35 -0400

    changed addUserToDatabase to only add if user does not exist in databse, configured a new tasklistview to display a users tasks in a list view

--- prog_tracker/lib/firebase.dart
+++ prog_tracker/lib/firebase.dart
@@ -3,13 +3,20 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/material.dart';
 
-class LoginAuth { 
+final GoogleSignIn _googleSignIn = GoogleSignIn();
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  static final GoogleSignIn _googleSignIn = GoogleSignIn();
-  static final FirebaseAuth _auth = FirebaseAuth.instance; 
+FirebaseUser _user;
+String _userID;
 
-  static Future<FirebaseUser> getUser() {
-    return _auth.currentUser();
+class LoginAuth {
+  static void _setUser() async {
+    _user = await _auth.currentUser();
+    _userID = _user.uid.toString();
+  }
+
+  static Future<FirebaseUser> getUser() async {
+    return await _auth.currentUser();
   }
 
   static Future<void> signOut() async {
@@ -20,7 +27,7 @@ class LoginAuth {
     }
   }
 
- static Future<void> signInAnonymously() async {
+  static Future<void> signInAnonymously() async {
     try {
       await FirebaseAuth.instance.signInAnonymously();
     } catch (e) {
@@ -30,21 +37,25 @@ class LoginAuth {
 
   static Future<FirebaseUser> signInGoogle() async {
     final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
 
     final AuthCredential credential = GoogleAuthProvider.getCredential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
 
-    final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;  
+    final FirebaseUser user =
+        (await _auth.signInWithCredential(credential)).user;
+    Database.addUserToDatabase(user);
+    _setUser();
     return user;
   }
 
-  static Future<FirebaseUser> handleSignInEmail(String _email, String _password) async {
+  static Future<FirebaseUser> handleSignInEmail(
+      String _email, String _password) async {
     AuthResult result = await _auth.signInWithEmailAndPassword(
-      email: _email.trim(), password: _password.trim()
-    );
+        email: _email.trim(), password: _password.trim());
     final FirebaseUser user = result.user;
 
     assert(user != null);
@@ -52,49 +63,82 @@ class LoginAuth {
 
     final FirebaseUser currentUser = await _auth.currentUser();
     assert(user.uid == currentUser.uid);
-
+    _setUser();
     return user;
   }
 
-  static Future<FirebaseUser> handleSignUp(String _email, String _password) async {
+  static Future<FirebaseUser> handleSignUp(
+      String _email, String _password) async {
     AuthResult result = await _auth.createUserWithEmailAndPassword(
-      email: _email.trim(), password: _password.trim()
-    );
+        email: _email.trim(), password: _password.trim());
     final FirebaseUser user = result.user;
 
-    assert (user != null);
-    assert (await user.getIdToken() != null);
+    assert(user != null);
+    assert(await user.getIdToken() != null);
 
     Database.addUserToDatabase(user);
+    _setUser();
     return user;
-  } 
+  }
 }
 
 class Database {
+  static void addUserToDatabase(FirebaseUser _user) {
+    print(_user.uid);
+    print(_user.uid.toString());
 
- static void addUserToDatabase (FirebaseUser _user){
-   print(_user.uid);
-   print(_user.uid.toString());
-   // if user already exist it will update it
-   Firestore.instance.collection("users").document(_user.uid).setData(
-     {
-      "name" : _user.uid.toString(),
-      "score" : 0
-     }
-   );
-   Firestore.instance.collection("users").document(_user.uid).collection("tasks").add(
-     {  
-      "task_name" : "placeholder_task_name",
-      "task_due" : "overdue",
-      "time_spent" : 0,
-      "completed" : false,
+    // if user not in database add them
+    Firestore.instance
+        .collection("users")
+        .document(_user.uid)
+        .get()
+        .then((value) {
+      if (!value.exists) {
+        Firestore.instance.collection("users").document(_user.uid).setData(
+            {"name": _user.uid.toString(), "score": 0},
+            merge: false // merge true overrites data
+            );
+        Firestore.instance
+            .collection("users")
+            .document(_user.uid)
+            .collection("tasks")
+            .add({
+          "task_name": "placeholder_task_name",
+          "task_due": "overdue",
+          "time_spent": 0,
+          "completed": false,
+        });
+      }
+    });
+  }
+}
 
-
-
-     }
-   );
-
-
- }
-
+class TasksListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance
+          .collection('users')
+          .document(_userID)
+          .collection("tasks")
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return new Text('Loading...');
+          default:
+            return new ListView(
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
+                return new ListTile(
+                  title: new Text(document['name']),
+                  // subtitle: new Text(document['task_due'].toString()),
+                );
+              }).toList(),
+            );
+        }
+      },
+    );
+  }
 }
--- prog_tracker/lib/home.dart
+++ prog_tracker/lib/home.dart
@@ -1,70 +1,55 @@
 import 'package:flutter/material.dart';
 import 'graph.dart';
 import 'leaderboard.dart';
+import 'firebase.dart';
 import 'clock.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green,
       appBar: AppBar(
         title: ClockWidget(),
-        actions: <Widget> [
+        actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.emoji_events), 
-            onPressed: () {  print('icon selected\n'); },
-                        
+            icon: Icon(Icons.emoji_events),
+            onPressed: () {
+              print('icon selected\n');
+            },
           )
         ],
-
       ),
-
-
       body: Container(
-        color: Colors.red,
-        child: Column(
-           
-           children: <Widget> [
-            Expanded(
-              child: 
-              Container(  
-                child: GaugeChart.withSampleData(),
+          color: Colors.red,
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  child: GaugeChart.withSampleData(),
+                ),
               ),
-            ),
-            
-
-             Expanded(
-                            child: Container(               
-                 color: Colors.yellow,
-                 child: DataBaseListView(),
-               ),
-             )
-           ],
-
-
-
-        ) 
-
-        ),
-
-      );
-        
+              RaisedButton(
+                onPressed: null,
+                color: Colors.grey,
+                child: Text(
+                  "Start",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: TasksListView(),
+                ),
+              )
+            ],
+          )),
+    );
   }
-
 }
-
-
-
-
-
-
-
-
