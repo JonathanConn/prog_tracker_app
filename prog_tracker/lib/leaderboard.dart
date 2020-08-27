e56b051ee9ff56a11a6a4bedf35ea00b1324e976
Jonathan <jonathanconn29@gmail.com>
2020-08-26 20:32:55 -0400

    added tabBar for navigation on leaderboardspage

--- prog_tracker/lib/leaderboard.dart
+++ prog_tracker/lib/leaderboard.dart
@@ -1,32 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class LeaderboardPage extends StatelessWidget {
+class Choice {
+  const Choice({this.title, this.icon});
+  final String title;
+  final IconData icon;
+}
+
+const List<Choice> choices = const <Choice>[
+  const Choice(title: 'Day', icon: Icons.timer),
+  const Choice(title: 'Week', icon: Icons.date_range),
+  const Choice(title: 'Month', icon: Icons.calendar_today),
+  const Choice(title: 'Year', icon: Icons.cake),
+];
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.choice}) : super(key: key);
+
+  final Choice choice;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold( 
-      body: Container( 
-        child: DataBaseListView(),
+    final TextStyle textStyle = Theme.of(context).textTheme.headline4;
+    return Card(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Icon(choice.icon, size: 128.0, color: textStyle.color),
+            Text(choice.title, style: textStyle),
+          ],
+        ),
       ),
     );
   }
 }
 
-class DataBaseListView extends StatelessWidget{
+class LeaderboardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: choices.length,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: TabBar(
+              isScrollable: true,
+              tabs: choices.map((Choice choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: choices.map((Choice choice) {
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ChoiceCard(choice: choice),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
- @override
+class DataBaseListView extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance.collection('users').snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError)
-          return new Text('Error: ${snapshot.error}');
+        if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
         switch (snapshot.connectionState) {
-          case ConnectionState.waiting: return new Text('Loading...');
+          case ConnectionState.waiting:
+            return new Text('Loading...');
           default:
             return new ListView(
-              children: snapshot.data.documents.map((DocumentSnapshot document) {
+              children:
+                  snapshot.data.documents.map((DocumentSnapshot document) {
                 return new ListTile(
                   title: new Text(document['name']),
                   subtitle: new Text(document['score'].toString()),
@@ -37,6 +95,4 @@ class DataBaseListView extends StatelessWidget{
       },
     );
   }
-
-
 }
